0abc7131f11385e01a1dab40eff3e1dcbe42b56b
RocktimSaikia <srocktim61@gmail.com>
2019-11-01 16:13:56 -0700

    config file with details addded :rocket:

--- components/Profile.js
+++ components/Profile.js
@@ -1,144 +1,155 @@
+import React from "react";
 import Fade from "react-reveal/Fade";
 
-export default () => (
-  <Fade duration={2000}>
-    <div className="profile">
-      <img src="https://avatars1.githubusercontent.com/rocktimsaikia" />
-      <div className="info">
-        <h3>Rocktim Saikia</h3>
-        <p className="location">
-          <i className="fas fa-map-marker-alt"></i> Assam ,India
-        </p>
-        <p className="email">
-          <i className="fas fa-envelope"></i> rocktim61@gmail.com
-        </p>
-        <p>Fullstack web developer</p>
+const checkUrls = obj => {
+  switch (obj.name) {
+    case "github":
+      return `https://github.com/${obj.userName}`;
+    case "twitter":
+      return `https://twitter.com/${obj.userName}`;
+    case "linkedin":
+      return `https://www.linkedin.com/in/${obj.userName}`;
+    case "instagram":
+      return `https://www.instagram.com/${obj.userName}`;
+    default:
+      return null;
+  }
+};
 
-        <div className="tech-stack">
-          <p>
-            <span className="dot"></span> Javascript
+export default ({
+  Config: {
+    GITHUB_USER_NAME,
+    NAME,
+    ROLE,
+    EMAIL,
+    TECH_STACK,
+    SOCIAL_MEDIA,
+    LOCATION
+  }
+}) => {
+  return (
+    <Fade duration={2000}>
+      <div className="profile">
+        <img
+          src={`https://avatars1.githubusercontent.com/${GITHUB_USER_NAME}`}
+        />
+        <div className="info">
+          <h3>{NAME}</h3>
+          <p className="location">
+            <i className="fas fa-map-marker-alt"></i>
+            {`${LOCATION.state}, ${LOCATION.country}`}
           </p>
-          <p>
-            <span className="dot"></span> React.js
+          <p className="email">
+            <i className="fas fa-envelope"></i> {EMAIL}
           </p>
-          <p>
-            <span className="dot"></span> Node.js
-          </p>
-          <p>
-            <span className="dot"></span> Next.js
-          </p>
+          <p>{ROLE}</p>
+
+          <div className="tech-stack">
+            {TECH_STACK.map(i => (
+              <p>
+                <span className="dot"></span> {i}
+              </p>
+            ))}
+          </div>
         </div>
+        <ul className="social">
+          {SOCIAL_MEDIA.map(i => (
+            <li>
+              <a href={checkUrls(i)} target="blank">
+                <i className={`fab fa-${i.name}`}></i>
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul className="social">
-        <li>
-          <a href="https://github.com/RocktimSaikia" target="blank">
-            <i className="fab fa-github"></i>
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/RocktimSaikia10" target="blank">
-            <i className="fab fa-twitter"></i>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/rocktim-saikia/" target="blank">
-            <i className="fab fa-linkedin"></i>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/itz_rocktim" target="blank">
-            <i className="fab fa-instagram"></i>
-          </a>
-        </li>
-      </ul>
-    </div>
-    <style jsx>{`
-      .profile {
-        position: fixed;
-        z-index: 1;
-        padding: 10px;
-        font-size: 1rem;
-        text-align: center;
-        box-shadow: rgba(0, 0, 0, 0.12) 0px 30px 60px;
-        background: #fff;
-        width: 190px;
-        height: 600px;
-        border-radius: 10px;
-      }
-      .tech-stack {
-        text-align: left;
-      }
-
-      .location > i {
-        color: #000;
-      }
-      .email > i {
-        color: #000;
-      }
-      .tech-stack > p {
-        margin: 10px;
-        padding: 5px 12px;
-        border-radius: 20px;
-        box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
-          0 4px 6px -2px rgba(0, 0, 0, 0.05);
-        background: #fdfdfd;
-      }
-      .dot {
-        height: 10px;
-        width: 10px;
-        background-color: #0070f399;
-        border-radius: 50%;
-        display: inline-block;
-        margin-right: 20px;
-      }
-      img {
-        height: 180px;
-        border-radius: 10px;
-      }
-      .social {
-        list-style: none;
-        padding: 0;
-      }
-      .social > li {
-        display: inline-block;
-        margin-right: 10px;
-        background: #6200edb8;
-        padding: 8px 10px;
-        border-radius: 50%;
-        box-shadow: 0 4px 14px 0 rgba(0, 118, 255, 0.39);
-      }
-      .social > li > a {
-        color: #fff;
-      }
-      .info {
-        text-align: left;
-        padding: 10px;
-      }
-      @media (max-width: 800px) {
+      <style jsx>{`
         .profile {
-          position: relative;
-          z-index: 0;
-          width: 100%;
-          height: 100%;
-          padding: 0;
+          position: fixed;
+          z-index: 1;
+          padding: 10px;
+          font-size: 1rem;
+          text-align: center;
+          box-shadow: rgba(0, 0, 0, 0.12) 0px 30px 60px;
+          background: #fff;
+          width: 190px;
+          height: 600px;
+          border-radius: 10px;
+        }
+        .tech-stack {
+          text-align: left;
+        }
+
+        .location > i {
+          color: #000;
+        }
+        .email > i {
+          color: #000;
+        }
+        .tech-stack > p {
+          margin: 10px;
+          padding: 5px 12px;
+          border-radius: 20px;
+          box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+            0 4px 6px -2px rgba(0, 0, 0, 0.05);
+          background: #fdfdfd;
+        }
+        .dot {
+          height: 10px;
+          width: 10px;
+          background-color: #0070f399;
+          border-radius: 50%;
+          display: inline-block;
+          margin-right: 20px;
         }
         img {
-          height: 120px;
-          float: left;
-          padding: 10px;
+          height: 180px;
+          border-radius: 10px;
+        }
+        .social {
+          list-style: none;
+          padding: 0;
+        }
+        .social > li {
+          display: inline-block;
+          margin-right: 10px;
+          background: #6200edb8;
+          padding: 8px 10px;
+          border-radius: 50%;
+          box-shadow: 0 4px 14px 0 rgba(0, 118, 255, 0.39);
+        }
+        .social > li > a {
+          color: #fff;
         }
         .info {
           text-align: left;
-          padding: 0;
-          margin: 0;
+          padding: 10px;
         }
-        .info > h3 {
-          margin-top: 6px;
+        @media (max-width: 800px) {
+          .profile {
+            position: relative;
+            z-index: 0;
+            width: 100%;
+            height: 100%;
+            padding: 0;
+          }
+          img {
+            height: 120px;
+            float: left;
+            padding: 10px;
+          }
+          .info {
+            text-align: left;
+            padding: 0;
+            margin: 0;
+          }
+          .info > h3 {
+            margin-top: 6px;
+          }
+          .social {
+            padding-bottom: 20px;
+          }
         }
-        .social {
-          padding-bottom: 20px;
-        }
-      }
-    `}</style>
-  </Fade>
-);
+      `}</style>
+    </Fade>
+  );
+};
---
+++ config/index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  GITHUB_USER_NAME: "rocktimsaikia",
+  NAME: "Rocktim Saikia",
+  ROLE: "Fullstack Web Developer",
+  EMAIL: "rocktim61@gmail.com",
+  LOCATION: {
+    state: "Assam",
+    country: "India"
+  },
+  TECH_STACK: ["Javascript", "React.js", "Node.js", "Next.js"],
+  SOCIAL_MEDIA: [
+    {
+      name: "github",
+      userName: "RocktimSaikia"
+    },
+    {
+      name: "twitter",
+      userName: "RocktimSaikia10"
+    },
+    {
+      name: "linkedin",
+      userName: "rocktim-saikia"
+    },
+    {
+      name: "instagram",
+      userName: "itz_rocktim"
+    }
+  ]
+};
